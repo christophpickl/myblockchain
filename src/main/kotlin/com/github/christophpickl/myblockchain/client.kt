fd0706640c584cbed80b93ecca8565c5ac20e150
Christoph Pickl <christoph.pickl@erstegroup.com>
2017-07-14 10:39:03 +0200

    client infra works

--- src/main/kotlin/com/github/christophpickl/myblockchain/client.kt
+++ src/main/kotlin/com/github/christophpickl/myblockchain/client.kt
@@ -1,49 +1,101 @@
 package com.github.christophpickl.myblockchain
 
 import com.github.christophpickl.kpotpourri.common.logging.LOG
+import com.google.common.eventbus.EventBus
 import com.google.inject.AbstractModule
 import com.google.inject.Guice
+import com.google.inject.Inject
 import javafx.application.Application
-import javafx.scene.text.FontWeight
 import tornadofx.App
+import tornadofx.Controller
+import tornadofx.DIContainer
+import tornadofx.EventBus.RunOn.BackgroundThread
+import tornadofx.FX
+import tornadofx.FXEvent
 import tornadofx.Stylesheet
 import tornadofx.View
-import tornadofx.c
-import tornadofx.hbox
+import tornadofx.addClass
+import tornadofx.button
+import tornadofx.cssclass
 import tornadofx.label
 import tornadofx.px
+import tornadofx.vbox
+import kotlin.reflect.KClass
 
 private val log = LOG {}
 
 fun main(args: Array<String>) {
-    log.info("Starting up guice.")
-    val guice = Guice.createInjector(ClientModule())
-    val client = guice.getInstance(ClientApp::class.java)
-    client.start(args)
+    log.info("Starting up client.")
+    Application.launch(ClientTornadoApp::class.java, *args)
 }
 
 class ClientModule : AbstractModule() {
     override fun configure() {
-        bind(ClientApp::class.java)
+        bind(CryptService::class.java).asEagerSingleton()
+        bind(MainController::class.java).asEagerSingleton()
     }
 }
 
-class ClientApp {
+class ClientTornadoApp : App(MainView::class, Styles::class) {
+    init {
+        val guice = Guice.createInjector(ClientModule())
+        FX.dicontainer = object : DIContainer {
+            override fun <T : Any> getInstance(type: KClass<T>) =
+                    guice.getInstance(type.java)
 
-    private val log = LOG {}
-
-    fun start(args: Array<String>) {
-        log.info("Starting up UI.")
-        Application.launch(ClientViewStarter::class.java, *args)
+        }
     }
-
 }
-
-class ClientViewStarter : App(MainView::class)
 
 class MainView : View() {
-    override val root = hbox {
+
+    override val root = vbox {
         label("Hello MyBlockchain")
+        button("Generate key pair") {
+            setOnAction {
+                fire(GenerateKeyPairRequest)
+            }
+        }
     }
 
+    init {
+        root.addClass(Styles.mainViewCssClass)
+    }
+}
+
+class CryptService {
+    private val log = LOG {}
+    fun createKeys() {
+        log.info("createKeys()")
+    }
+}
+
+class MainController @Inject constructor(
+        private val cryptService: CryptService
+) : Controller() {
+    private val logg = LOG {}
+    private val mainView: MainView by inject()
+    // private val cryptService: CryptService by di()
+
+    init {
+        subscribe<GenerateKeyPairRequest> {
+            logg.debug("subscribed event dispatched: GenerateKeyPairRequest")
+            cryptService.createKeys()
+        }
+    }
+
+}
+object GenerateKeyPairRequest : FXEvent(BackgroundThread)
+
+class Styles : Stylesheet() {
+    companion object {
+        val mainViewCssClass by cssclass()
+    }
+    init {
+        select(mainViewCssClass) {
+            padding = tornadofx.box(15.px)
+            vgap = 7.px
+            hgap = 10.px
+        }
+    }
 }
