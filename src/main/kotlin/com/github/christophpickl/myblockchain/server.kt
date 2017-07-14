805410be6df1574f04f174ca6aefce50aec72455
Christoph Pickl <christoph.pickl@erstegroup.com>
2017-07-14 10:04:25 +0200

    server starting up

---
+++ src/main/kotlin/com/github/christophpickl/myblockchain/server.kt
@@ -0,0 +1,31 @@
+package com.github.christophpickl.myblockchain
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+class MyApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(MyApplication::class.java, *args)
+}
+
+@RestController
+class MyController {
+    @RequestMapping("/")
+    fun index(): String {
+        return "hallo"
+    }
+
+    @RequestMapping("/transaction")
+    fun transaction() = listOf(
+            Transaction("id1"),
+            Transaction("id2")
+    )
+}
+
+data class Transaction(
+        val id: String
+)
