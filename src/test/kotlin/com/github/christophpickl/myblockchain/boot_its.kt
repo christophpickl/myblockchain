3e50a9098ba51ae3accd2f7313cdaaf52998f35b
christophpickl <christoph.pickl@gmail.com>
2017-07-15 07:48:14 +0200

    spring boot IT working

---
+++ src/test/kotlin/com/github/christophpickl/myblockchain/boot_its.kt
@@ -0,0 +1,37 @@
+package com.github.christophpickl.myblockchain
+
+import com.github.christophpickl.kpotpourri.http4k.Http4k
+import com.github.christophpickl.kpotpourri.http4k.buildHttp4k
+import com.github.christophpickl.kpotpourri.http4k.get
+import com.github.christophpickl.myblockchain.server.MyBlockchainSpringBootApplication
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit4.SpringRunner
+
+
+// https://docs.spring.io/spring-boot/docs/current/reference/html/boot-features-testing.html
+
+@RunWith(SpringRunner::class)
+@SpringBootTest(webEnvironment = DEFINED_PORT,
+        properties = arrayOf("server.port=8080", "spring.main.banner-mode=off"))
+@ContextConfiguration(classes = arrayOf(MyBlockchainSpringBootApplication::class))
+class FooIT {
+
+    private val http4k: Http4k by lazy {
+        buildHttp4k {
+            baseUrlBy("http://localhost:8080")
+        }
+    }
+
+    @Test
+    fun `GET node ip - should return localhost's IP address`() {
+       assertThat(http4k.get<String>("/node/ip"),
+               equalTo("127.0.0.1"))
+    }
+
+}
